98c6ef1de7425b6bb4ddc4027f2945a79a78a729
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-03 17:49:42 -0400

    Update and rename src/Driver.java to src/p2/Driver.java

---
+++ src/p2/Driver.java
@@ -0,0 +1,32 @@
+package cs1302.p2;
+
+import cs1302.effects.Artsy;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+
+/**
+ * This is the driver for this application.
+ */
+public class Driver extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        FlowPane root = new FlowPane();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Artsy!");
+        stage.sizeToScene();
+        stage.show();
+        
+    } // createAndShowGUI
+
+    public static void main(String[] args) {
+        launch(args);
+    } // main
+
+} // Driver
