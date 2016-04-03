3b270e85a488925d625ad180cf5ed7d45a083b9c
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-03 17:50:12 -0400

    Update MyArtsy.java

--- src/MyArtsy.java
+++ src/MyArtsy.java
@@ -1,1 +1,30 @@
+package cs1302.p2;
+
+import cs1302.effects.Artsy;
+import javafx.scene.image.Image;
+
+public class MyArtsy implements Artsy {
+
+    @Override
+    public Image doCheckers(Image src1, Image src2, int size) {
+	return null;
+    } // doCheckers
+
+    @Override
+    public Image doHorizontalStripes(Image src1, Image src2, int height) {
+	return null;
+    } // doHorizontalStripes
+
+    @Override
+    public Image doRotate(Image src, double degrees) {
+	return null;
+    } // doRotate
+
+    @Override
+    public Image doVerticalStripes(Image src1, Image src2, int width) {
+	return null;
+    } // doVerticalStripes
+
+} // MyArtsy
+
 
