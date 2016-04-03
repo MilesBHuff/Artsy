43083e3087e2491877cd982ea37e02a71eb94335
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-03 18:31:38 -0400

    Update Artsy.java

--- src/effects/Artsy.java
+++ src/effects/Artsy.java
@@ -2,29 +2,34 @@ package src.effects;
 import  javafx.scene.image.Image;
 
 /** This interface describes artsy effects that can be applied to Images. **/
-public interface Artsy {
-
-	/** Given two Images, this method returns a new Image, the contents of which
-	 *  is composed of the source images alternating horizontally and vertically at the specified 
-	 *  size, in pixels. This causes the images to appear woven together in a checkered fashion. 
-	 *  For an example, please see the project description.
+public interface Artsy
+{ //interface
+	////////////////////////////////////////////////////////////////////////
+	/** Given two Images, this method returns a new Image, the contents of
+	 *  which is composed of the source images alternating horizontally and
+	 *  vertically at the specified size, in pixels. This causes the images
+	 *  to appear woven together in a checkered fashion.  For an example,
+	 *  please see the project description.
 	 * @param  src1   The first Image
 	 * @param  src2   The second Image
 	 * @param  height The height, in pixels, of the horizontal stripes.
-	 * @return an <code>Image</code> with the horizontal stripes effect
+	 * @return an <code>Image</code> with the horizontal stripes effect.
 	**/
 	public Image doCheckers(Image src1, Image src2, int size);
-	
-	/** Given two Images, this method returns a new Image, the contents of which
-	 *  is composed of the source images alternating horizontally at the specified pixel height. 
-	 *  For an example, please see the project description.
+
+	////////////////////////////////////////////////////////////////////////
+	/** Given two Images, this method returns a new Image, the contents of
+	 *  which is composed of the source images alternating horizontally at
+	 *  the specified pixel height.  For an example, please see the project
+	 *  description.
 	 * @param  src1   The first Image
 	 * @param  src2   The second Image
 	 * @param  height The height, in pixels, of the horizontal stripes.
-	 * @return an <code>Image</code> with the horizontal stripes effect
+	 * @return an <code>Image</code> with the horizontal stripes effect.
 	**/
 	public Image doHorizontalStripes(Image src1, Image src2, int height);
-	
+
+	////////////////////////////////////////////////////////////////////////
 	/** Given two Images, this method returns a new Image, the contents of which
 	 *  is composed of the source images alternating vertically at the specified pixel width. For
 	 *  an example, please see the project description.
@@ -34,7 +39,8 @@ public interface Artsy {
 	 * @return an <code>Image</code> with the vertical stripes effect
 	 */
 	public Image doVerticalStripes(Image src1, Image src2, int width);
-	
+
+	////////////////////////////////////////////////////////////////////////
 	/** Given a Image, this method returns a new Image which contains
 	 *  the source image rotated clockwise by a certain number of degrees. The image
 	 *  should be rotated about the center of the image. For an example, please see
@@ -48,5 +54,4 @@ public interface Artsy {
 	 * @return a new <code>Image</code> containing a rotated version of the source image
 	 */
 	public Image doRotate(Image src, double degrees);
-	
-} // Artsy
+} //interface
