8befdbcc3db08548afe7ae5c5b3008271e6edb91
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-06 18:25:43 -0400

    Update Driver.java

--- src/p2/Driver.java
+++ src/p2/Driver.java
@@ -15,7 +15,7 @@ public class Driver extends Application
 	 * @param stage $Description
 	**/
 	@Override
-	public void start(Stage oStage)
+	public void start(Stage stgRoot)
 	{ //method
 		// DATA
 		String sBtnRotate  = "Rotate"             ;
@@ -31,70 +31,70 @@ public class Driver extends Application
 		String sLblResult  = "Result:"            ;
 
 		// IMG1 BUTTONS
-		GridPane oImage1Btns = new GridPane();
+		GridPane gpImg1Btns = new GridPane();
+		Button   btnRotate1 = new Button(sBtnRotate); gpImg1Btns.add(btnRotate1, 0, 0);
+		Button   btnClose1  = new Button(sBtnClose) ; gpImg1Btns.add(btnClose1 , 1, 0);
+		Button   btnOpen1   = new Button(sBtnOpen)  ; gpImg1Btns.add(btnOpen1  , 2, 0);
+		Button   btnSave1   = new Button(sBtnSave)  ; gpImg1Btns.add(btnSave1  , 3, 0);
 		oImageBtns.setHgap(10);
 		oImageBtns.setVgap(10);
-		Button oBtnRotate1 = new Button(sBtnRotate); oEffectBtns.add(oBtnRotate1, 0, 0);
-		Button oBtnClose1  = new Button(sBtnClose) ; oEffectBtns.add(oBtnClose1 , 1, 0);
-		Button oBtnOpen1   = new Button(sBtnOpen)  ; oEffectBtns.add(sBtnOpen1  , 2, 0);
-		Button oBtnSave1   = new Button(sBtnSave)  ; oEffectBtns.add(oBtnSave1  , 3, 0);
 		
 		// IMG2 BUTTONS
-		GridPane oImage2Btns = new GridPane();
+		GridPane gpImg2Btns = new GridPane();
+		Button   btnRotate2 = new Button(sBtnRotate); gpImg2Btns.add(btnRotate2, 0, 0);
+		Button   btnClose2  = new Button(sBtnClose) ; gpImg2Btns.add(btnClose2 , 1, 0);
+		Button   btnOpen2   = new Button(sBtnOpen)  ; gpImg2Btns.add(btnOpen2  , 2, 0);
+		Button   btnSave2   = new Button(sBtnSave)  ; gpImg2Btns.add(btnSave2  , 3, 0);
 		oImageBtns.setHgap(10);
 		oImageBtns.setVgap(10);
-		Button oBtnRotate2 = new Button(sBtnRotate); oEffectBtns.add(oBtnRotate2, 0, 0);
-		Button oBtnClose2  = new Button(sBtnClose) ; oEffectBtns.add(oBtnClose2 , 1, 0);
-		Button oBtnOpen2   = new Button(sBtnOpen)  ; oEffectBtns.add(sBtnOpen2  , 2, 0);
-		Button oBtnSave2   = new Button(sBtnSave)  ; oEffectBtns.add(oBtnSave2  , 3, 0);
 		
 		// IMG3 BUTTONS
-		GridPane oImage3Btns = new GridPane();
+		GridPane gpImg3Btns = new GridPane();
+		Button   btnRotate3 = new Button(sBtnRotate); gpImg3Btns.add(btnRotate3, 0, 0);
+		Button   btnClose3  = new Button(sBtnClose) ; gpImg3Btns.add(btnClose3 , 1, 0);
+		Button   btnOpen3   = new Button(sBtnOpen)  ; gpImg3Btns.add(btnOpen3  , 2, 0);
+		Button   btnSave3   = new Button(sBtnSave)  ; gpImg3Btns.add(btnSave3  , 3, 0);
 		oImageBtns.setHgap(10);
 		oImageBtns.setVgap(10);
-		Button oBtnRotate3 = new Button(sBtnRotate); oEffectBtns.add(oBtnRotate3, 0, 0);
-		Button oBtnClose3  = new Button(sBtnClose) ; oEffectBtns.add(oBtnClose3 , 1, 0);
-		Button oBtnOpen3   = new Button(sBtnOpen)  ; oEffectBtns.add(sBtnOpen3  , 2, 0);
-		Button oBtnSave3   = new Button(sBtnSave)  ; oEffectBtns.add(oBtnSave3  , 3, 0);
 
 		// EFFECTS BUTTONS
-		GridPane oEffectBtns = new GridPane();
-		oEffectBtns.setHgap(10);
-		oEffectBtns.setVgap(10);
-		Button oBtnChecker = new Button(sBtnChecker); oEffectBtns.add(oBtnChecker, 0, 0);
-		Button oBtnStripeH = new Button(sBtnStripeH); oEffectBtns.add(oBtnStripeH, 0, 1);
-		Button oBtnStripeV = new Button(sBtnStripeV); oEffectBtns.add(oBtnStripeV, 0, 2);
+		GridPane gpEffectBtns = new GridPane();
+		Button btnChecker = new Button(sBtnChecker); gpEffectBtns.add(btnChecker, 0, 0);
+		Button btnStripeH = new Button(sBtnStripeH); gpEffectBtns.add(btnStripeH, 0, 1);
+		Button btnStripeV = new Button(sBtnStripeV); gpEffectBtns.add(btnStripeV, 0, 2);
+		gpEffectBtns.setHgap(10);
+		gpEffectBtns.setVgap(10);
 
 		// MAIN PANE
-		GridPane oRoot = new GridPane();
-		oRoot.setHgap(10);
-		oRoot.setVgap(10);
-		oRoot.setPadding(new Insets(25, 25, 25, 25));
+		GridPane gpRoot = new GridPane();
+		gpRoot.setPadding(new Insets(25, 25, 25, 25));
+		gpRoot.setHgap(10);
+		gpRoot.setVgap(10);
 		
 		// MENUBAR
-		MenuBar mbrMenu = new MenuBar()   ; oRoot.add(mbrMenu, 0, 0, 2, 1);
-	        Menu    menFile = new Menu("File");
-	        Menu    menHelp = new Menu("Help");
-	        menuBar.getMenus().addAll(menFile, menHelp, menuView);
+		MenuBar mbrMenu = new MenuBar()        ; gpRoot.add(mbrMenu, 0, 0, 2, 1);
+	        Menu    menFile = new Menu   ("File")  ;
+	        Menu    menHelp = new Menu   ("Help")  ;
+	        menuBar.getMenus().addAll(menFile, menHelp);
 	        
 	        // OTHER ITEMS ON THE MAIN PANE
-		Label lblImage1 = new Label(sLblImage1); oRoot.add(lblImage1  , 0, 1      );
+		Label lblImage1 = new Label(sLblImage1); gpRoot.add(lblImage1   , 0, 1      );
 		// Img1 (0, 2)
-		                                         oRoot.add(img1Btns   , 0, 3      );
-		Label lblImage2 = new Label(sLblImage2); oRoot.add(lblImage2  , 0, 4      );
+		                                         gpRoot.add(gpImg1Btns  , 0, 3      );
+		Label lblImage2 = new Label(sLblImage2); gpRoot.add(lblImage2   , 0, 4      );
 		// Img2 (0, 5)
-		                                         oRoot.add(img2Btns   , 0, 6      );
-		Label lblEffect = new Label(sLblEffect); oRoot.add(lblEffect  , 1, 1      );
-		                                         oRoot.add(oEffectBtns, 1, 2, 1, 2);
-		Label lblResult = new Label(sLblResult); oRoot.add(lblResult  , 1, 4      );
+		                                         gpRoot.add(gpImg2Btns  , 0, 6      );
+		Label lblEffect = new Label(sLblEffect); gpRoot.add(lblEffect   , 1, 1      );
+		                                         gpRoot.add(gpEffectBtns, 1, 2, 1, 2);
+		Label lblResult = new Label(sLblResult); gpRoot.add(lblResult   , 1, 4      );
 		// Img3 (1, 5)
-		                                         oRoot.add(oImage3Btns, 1, 6      );
+		                                         gpRoot.add(gpImg3Btns  , 1, 6      );
 
-		Scene oScene = new Scene(oRoot);
-		oStage.setScene(oScene);
-		oStage.setTitle("Artsy!");
-	        oStage.sizeToScene();
-		oStage.show();
+		Scene sceRoot = new Scene(gpRoot);
+		stgRoot.setScene(sceRoot);
+		stgRoot.setTitle("Artsy!");
+	        stgRoot.sizeToScene();
+		stgRoot.show();
 	} //method
 
 	////////////////////////////////////////////////////////////////////////
