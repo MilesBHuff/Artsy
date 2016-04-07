287b173d87fee474f9c743069b6658567121e013
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-06 22:25:17 -0400

    Update Driver.java

--- src/p2/Driver.java
+++ src/p2/Driver.java
@@ -32,44 +32,44 @@ public class Driver extends Application
 
 		// IMG1 BUTTONS
 		GridPane gpImg1Btns = new GridPane(          );
-		Button   btnRotate1 = new Button  (sBtnRotate); gpImg1Btns.add(btnRotate1, 0, 0);
-		Button   btnClose1  = new Button  (sBtnClose ); gpImg1Btns.add(btnClose1 , 1, 0);
-		Button   btnOpen1   = new Button  (sBtnOpen  ); gpImg1Btns.add(btnOpen1  , 2, 0);
-		Button   btnSave1   = new Button  (sBtnSave  ); gpImg1Btns.add(btnSave1  , 3, 0);
+		Button  btnRotate1  = new Button  (sBtnRotate); gpImg1Btns.add(btnRotate1, 0, 0);
+		Button  btnClose1   = new Button  (sBtnClose ); gpImg1Btns.add(btnClose1 , 1, 0);
+		Button  btnOpen1    = new Button  (sBtnOpen  ); gpImg1Btns.add(btnOpen1  , 2, 0);
+		Button  btnSave1    = new Button  (sBtnSave  ); gpImg1Btns.add(btnSave1  , 3, 0);
 		
 		// IMG2 BUTTONS
 		GridPane gpImg2Btns = new GridPane(          );
-		Button   btnRotate2 = new Button  (sBtnRotate); gpImg2Btns.add(btnRotate2, 0, 0);
-		Button   btnClose2  = new Button  (sBtnClose ); gpImg2Btns.add(btnClose2 , 1, 0);
-		Button   btnOpen2   = new Button  (sBtnOpen  ); gpImg2Btns.add(btnOpen2  , 2, 0);
-		Button   btnSave2   = new Button  (sBtnSave  ); gpImg2Btns.add(btnSave2  , 3, 0);
+		Button  btnRotate2  = new Button  (sBtnRotate); gpImg2Btns.add(btnRotate2, 0, 0);
+		Button  btnClose2   = new Button  (sBtnClose ); gpImg2Btns.add(btnClose2 , 1, 0);
+		Button  btnOpen2    = new Button  (sBtnOpen  ); gpImg2Btns.add(btnOpen2  , 2, 0);
+		Button  btnSave2    = new Button  (sBtnSave  ); gpImg2Btns.add(btnSave2  , 3, 0);
 		
 		// IMG3 BUTTONS
 		GridPane gpImg3Btns = new GridPane(          );
-		Button   btnRotate3 = new Button  (sBtnRotate); gpImg3Btns.add(btnRotate3, 0, 0);
-		Button   btnClose3  = new Button  (sBtnClose ); gpImg3Btns.add(btnClose3 , 1, 0);
-		Button   btnOpen3   = new Button  (sBtnOpen  ); gpImg3Btns.add(btnOpen3  , 2, 0);
-		Button   btnSave3   = new Button  (sBtnSave  ); gpImg3Btns.add(btnSave3  , 3, 0);
+		Button  btnRotate3  = new Button  (sBtnRotate); gpImg3Btns.add(btnRotate3, 0, 0);
+		Button  btnClose3   = new Button  (sBtnClose ); gpImg3Btns.add(btnClose3 , 1, 0);
+		Button  btnOpen3    = new Button  (sBtnOpen  ); gpImg3Btns.add(btnOpen3  , 2, 0);
+		Button  btnSave3    = new Button  (sBtnSave  ); gpImg3Btns.add(btnSave3  , 3, 0);
 
 		// FX BUTTONS
-		GridPane gpFXBtns   = new GridPane(           );
-		Button   btnChecker = new Button  (sBtnChecker); gpEffectBtns.add(btnChecker, 0, 0);
-		Button   btnStripeH = new Button  (sBtnStripeH); gpEffectBtns.add(btnStripeH, 0, 1);
-		Button   btnStripeV = new Button  (sBtnStripeV); gpEffectBtns.add(btnStripeV, 0, 2);
+		GridPane gpFXBtns  = new GridPane(           );
+		Button  btnChecker = new Button  (sBtnChecker); gpEffectBtns.add(btnChecker, 0, 0);
+		Button  btnStripeH = new Button  (sBtnStripeH); gpEffectBtns.add(btnStripeH, 0, 1);
+		Button  btnStripeV = new Button  (sBtnStripeV); gpEffectBtns.add(btnStripeV, 0, 2);
 
 		// MAIN PANE
-		GridPane gpRoot  = new GridPane(        );
-		Label    lblImg1 = new Label   (sLblImg1); gpRoot.add(lblImg1   , 0, 1      );
+		GridPane gpRoot = new GridPane(        );
+		Label   lblImg1 = new Label   (sLblImg1); gpRoot.add(lblImg1    , 0, 1      );
 		// Img1 (0, 2)
-		                                           gpRoot.add(gpImg1Btns, 0, 3      );
-		Label    lblImg2 = new Label   (sLblImg2); gpRoot.add(lblImg2   , 0, 4      );
+		                                          gpRoot.add( gpImg1Btns, 0, 3      );
+		Label   lblImg2 = new Label   (sLblImg2); gpRoot.add(lblImg2    , 0, 4      );
 		// Img2 (0, 5)
-		                                           gpRoot.add(gpImg2Btns, 0, 6      );
-		Label    lblFX   = new Label   (sLblFX  ); gpRoot.add(lblFX     , 1, 1      );
-		                                           gpRoot.add(gpFXBtns  , 1, 2, 1, 2);
-		Label    lblImg3 = new Label   (sLblImg3); gpRoot.add(lblImg3   , 1, 4      );
+		                                          gpRoot.add( gpImg2Btns, 0, 6      );
+		Label   lblFX   = new Label   (sLblFX  ); gpRoot.add(lblFX      , 1, 1      );
+		                                          gpRoot.add( gpFXBtns  , 1, 2, 1, 2);
+		Label   lblImg3 = new Label   (sLblImg3); gpRoot.add(lblImg3    , 1, 4      );
 		// Img3 (1, 5)
-		                                           gpRoot.add(gpImg3Btns, 1, 6      );
+		                                          gpRoot.add(gpImg3Btns, 1, 6      );
 		
 		// MENUBAR
 		MenuBar mbrRoot = new MenuBar(      ); gpRoot.add(mbrRoot, 0, 0, 2, 1);
@@ -78,17 +78,17 @@ public class Driver extends Application
 	        mbrRoot.getMenus().addAll(menFile, menHelp);
 
 		// SPACING
-		gpRoot      .setPadding(new Insets(25, 25, 25, 25));
-		gpRoot      .setHgap   (10);
-		gpRoot      .setVgap   (10);
-		gpEffectBtns.setHgap   (10);
-		gpEffectBtns.setVgap   (10);
-		gpImg3Btns  .setHgap   (10);
-		gpImg3Btns  .setVgap   (10);
-		gpImg2Btns  .setHgap   (10);
-		gpImg2Btns  .setVgap   (10);
-		gpImg1Btns  .setHgap   (10);
-		gpImg1Btns  .setVgap   (10);
+		gpFXBtns  .setHgap   (10);
+		gpFXBtns  .setVgap   (10);
+		gpImg1Btns.setHgap   (10);
+		gpImg1Btns.setVgap   (10);
+		gpImg2Btns.setHgap   (10);
+		gpImg2Btns.setVgap   (10);
+		gpImg3Btns.setHgap   (10);
+		gpImg3Btns.setVgap   (10);
+		gpRoot    .setHgap   (10);
+		gpRoot    .setVgap   (10);
+		gpRoot    .setPadding(new Insets(25, 25, 25, 25));
 		
 		// SET THE STAGE
 		Scene sceRoot = new Scene (gpRoot);
