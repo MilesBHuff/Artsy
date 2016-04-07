abee61b5242ac325d8016c18791b279f20e2b146
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-06 22:41:58 -0400

    Update Driver.java

--- src/p2/Driver.java
+++ src/p2/Driver.java
@@ -19,35 +19,47 @@ public class Driver extends Application
 	{ //method
 		// DATA
 		String sBtnChecker = "Checker"            ;
-		String sBtnClose   = "Close"              ;
-		String sBtnOpen    = "Open"               ;
-		String sBtnRotate  = "Rotate"             ;
-		String sBtnSave    = "Save"               ;
+	        String sBtnOpen    = ""                   ;
+	        String sBtnReset   = ""                   ;
+	        String sBtnRotate  = ""                   ;
+	        String sBtnSave    = ""                   ;
 		String sBtnStripeH = "Stripe horizontally";
 		String sBtnStripeV = "Stripe vertically"  ;
 		String sLblFX      = "Effects:"           ;
 		String sLblImg1    = "Image 1:  "         ;
 		String sLblImg2    = "Image 2:  "         ;
 		String sLblImg3    = "Result:"            ;
+		
+		// RESOURCES
+		Image img1       = new Image(getClass().getResourceAsStream("null.png"));
+		Image img2       = new Image(getClass().getResourceAsStream("null.png"));
+		Image img3       = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgChecker = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgOpen    = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgReset   = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgRotate  = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgSave    = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgStripeH = new Image(getClass().getResourceAsStream("null.png"));
+		Image imgStripeV = new Image(getClass().getResourceAsStream("null.png"));
 
 		// IMG1 BUTTONS
 		GridPane gpImg1Btns = new GridPane(          );
 		Button  btnRotate1  = new Button  (sBtnRotate); gpImg1Btns.add(btnRotate1, 0, 0);
-		Button  btnClose1   = new Button  (sBtnClose ); gpImg1Btns.add(btnClose1 , 1, 0);
+		Button  btnReset1   = new Button  (sBtnReset ); gpImg1Btns.add(btnReset1 , 1, 0);
 		Button  btnOpen1    = new Button  (sBtnOpen  ); gpImg1Btns.add(btnOpen1  , 2, 0);
 		Button  btnSave1    = new Button  (sBtnSave  ); gpImg1Btns.add(btnSave1  , 3, 0);
 		
 		// IMG2 BUTTONS
 		GridPane gpImg2Btns = new GridPane(          );
 		Button  btnRotate2  = new Button  (sBtnRotate); gpImg2Btns.add(btnRotate2, 0, 0);
-		Button  btnClose2   = new Button  (sBtnClose ); gpImg2Btns.add(btnClose2 , 1, 0);
+		Button  btnReset2   = new Button  (sBtnReset ); gpImg2Btns.add(btnReset2 , 1, 0);
 		Button  btnOpen2    = new Button  (sBtnOpen  ); gpImg2Btns.add(btnOpen2  , 2, 0);
 		Button  btnSave2    = new Button  (sBtnSave  ); gpImg2Btns.add(btnSave2  , 3, 0);
 		
 		// IMG3 BUTTONS
 		GridPane gpImg3Btns = new GridPane(          );
 		Button  btnRotate3  = new Button  (sBtnRotate); gpImg3Btns.add(btnRotate3, 0, 0);
-		Button  btnClose3   = new Button  (sBtnClose ); gpImg3Btns.add(btnClose3 , 1, 0);
+		Button  btnReset3   = new Button  (sBtnReset ); gpImg3Btns.add(btnReset3 , 1, 0);
 		Button  btnOpen3    = new Button  (sBtnOpen  ); gpImg3Btns.add(btnOpen3  , 2, 0);
 		Button  btnSave3    = new Button  (sBtnSave  ); gpImg3Btns.add(btnSave3  , 3, 0);
 
@@ -69,13 +81,30 @@ public class Driver extends Application
 		                                          gpRoot.add( gpFXBtns  , 1, 2, 1, 2);
 		Label   lblImg3 = new Label   (sLblImg3); gpRoot.add(lblImg3    , 1, 4      );
 		// Img3 (1, 5)
-		                                          gpRoot.add(gpImg3Btns, 1, 6      );
+		                                          gpRoot.add(gpImg3Btns, 1, 6       );
 		
 		// MENUBAR
 		MenuBar mbrRoot = new MenuBar(      ); gpRoot.add(mbrRoot, 0, 0, 2, 1);
 	        Menu    menFile = new Menu   ("File");
 	        Menu    menHelp = new Menu   ("Help");
 	        mbrRoot.getMenus().addAll(menFile, menHelp);
+	        
+	        // IMAGES
+		btnChecker.setGraphic(new ImageView(imgChecker));
+		btnOpen1  .setGraphic(new ImageView(impOpen   ));
+		btnOpen2  .setGraphic(new ImageView(impOpen   ));
+		btnOpen3  .setGraphic(new ImageView(impOpen   ));
+		btnReset1 .setGraphic(new ImageView(imgReset  ));
+		btnReset2 .setGraphic(new ImageView(imgReset  ));
+		btnReset3 .setGraphic(new ImageView(imgReset  ));
+		btnRotate1.setGraphic(new ImageView(imgRotate ));
+		btnRotate2.setGraphic(new ImageView(imgRotate ));
+		btnRotate3.setGraphic(new ImageView(imgRotate ));
+		btnSave1  .setGraphic(new ImageView(imgSave   ));
+		btnSave2  .setGraphic(new ImageView(imgSave   ));
+		btnSave3  .setGraphic(new ImageView(imgSave   ));
+		btnStripeH.setGraphic(new ImageView(imgStripeH));
+		btnStripeV.setGraphic(new ImageView(imgStripeV));
 
 		// SPACING
 		gpFXBtns  .setHgap   (10);
