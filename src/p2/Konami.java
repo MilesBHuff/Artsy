0b2bc2050026cf2826a28d54a862222076096960
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-10 18:11:36 -0400

    Update Konami.java

--- src/p2/Konami.java
+++ src/p2/Konami.java
@@ -1,10 +1,16 @@
+
+
 public class Konami()
-{
-        Queue queue = new ArrayBlockingQueue<String>(6);
+{ //+class
+        Queue que = new ArrayBlockingQueue<String>(6);
         public static boolean analyze(String sArg)
-        {
-        	queue.add(sArg);
-        	if(queue.toString().equals("11223434AB")) return true;
+        { //+method
+        	que.put(sArg);  // Adds sArg as soon as there's space in que
+                if(que.size() == 6)
+                { //+if
+                        que.take();  // Removes the head of the queue
+                	if(que.toString().equals("[1, 1, 2, 2, 3, 4, 3, 4, A, B]")) return true;
+                } //-if
         	return false;
-        }
-}
+        } //-method
+} //-class
