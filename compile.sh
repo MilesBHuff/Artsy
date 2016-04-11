bcdc325ddf1398fe4e18ba2de14254d56a5bb34a
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-11 11:36:30 -0400

    Delete compile.sh

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env sh
-## Script by Miles B Huff
-echo "Building 'Artsy'..."
-mkdir build && cd build
-javac ../src/*.java
-cd ..
-jar -cf Artsy.jar build/*.class
-#rm -rf build
-echo "Done.  "
