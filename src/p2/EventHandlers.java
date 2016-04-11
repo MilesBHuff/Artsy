21fdf1010eb98ca93d8f3e6ece3032a31289641d
Miles B Huff <Sweyn78@users.noreply.github.com>
2016-04-11 00:05:56 -0400

    Update EventHandlers.java

--- src/p2/EventHandlers.java
+++ src/p2/EventHandlers.java
@@ -1,1 +1,21 @@
+package src.p2;
 
+class EventHandlers
+{ //+class
+        open()
+        { //+method
+                
+        } //-method
+        save()
+        { //+method
+                
+        } //-method
+        reset()
+        { //+method
+                
+        } //-method
+        url()
+        { //+method
+                
+        } //-method
+} //-class
